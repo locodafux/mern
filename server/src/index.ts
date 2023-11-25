ea5f99f9d22d56a9b4824a13a2210963dfc9ede7
locodafux <locodafux@gmail.com>
2023-11-25 13:47:11 +0800

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import { config } from "dotenv";
+config();
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+
+app.post("/decks", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log("listening to port {PORT}");
+  app.listen(PORT);
+});
